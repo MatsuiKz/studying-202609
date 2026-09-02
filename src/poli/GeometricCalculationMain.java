0ba2a153de2711857ea3a4eef0b87651c4be4f9f
matsuikz <biz.matsuik@gmail.com>
2026-09-02 17:37:43 +0900

    各図形の面積を求める処理を追加

--- src/poli/GeometricCalculationMain.java
+++ src/poli/GeometricCalculationMain.java
@@ -6,53 +6,39 @@ import java.util.List;
  */
 public class GeometricCalculationMain {
     public static void main(String[] args) {
+
         // Shape型なのでListで受けられる
-        List<Shape> shapes = List.of(new CircleImpl(5.0), new TriangleImpl(1, 2), new SquareImpl(2, 3));
+        List<Shape> shapes = List.of(
+                new CircleImpl(5.0), new TriangleImpl(1, 2), new SquareImpl(2, 3));
+
+        areaSum(shapes);
+        areaIndividual(shapes);
+
+    }
+
+    private static void areaSum(List<Shape> shapes) {
 
         // 合計値を出す場合（当初要件ではないが）
         double total = 0;
-        for (Shape shape: shapes) {
+        for (Shape shape : shapes) {
             total += shape.getArea();
         }
-        
+
         System.out.println(total);
     }
 
-    /**
-     * 円の面積を計算する
-     * 
-     * @param radius
-     */
-    private static void calcCircleArea(double radius) {
-        // 円の面積
-        Shape circle = new CircleImpl(radius);
-        double area = circle.getArea();
-        System.out.println("円の面積: " + area);
-    }
+    private static void areaIndividual(List<Shape> shapes) {
 
-    /**
-     * 三角形の面積を計算する
-     * 
-     * @param bottom
-     * @param height
-     */
-    private static void calcTriangleArea(double bottom, double height) {
-        // 三角形の面積
-        TriangleImpl triangle = new TriangleImpl(bottom, height);
-        double area = triangle.getArea();
-        System.out.println("三角形の面積: " + area);
-    }
-
-    /**
-     * 四角形の面積を計算する
-     * 
-     * @param vertical
-     * @param horizontal
-     */
-    private static void calcSquareArea(double vertical, double horizontal) {
-        // 四角形の面積
-        SquareImpl square = new SquareImpl(vertical, horizontal);
-        double area = square.getArea();
-        System.out.println("四角形の面積: " + area);
+        for (Shape shape : shapes) {
+            if (shape instanceof CircleImpl) {
+                System.out.println("円の面積：" + String.valueOf(shape.getArea()));
+            }
+            if (shape instanceof TriangleImpl) {
+                System.out.println("三角形の面積：" + String.valueOf(shape.getArea()));
+            }
+            if (shape instanceof SquareImpl) {
+                System.out.println("四角形の面積：" + String.valueOf(shape.getArea()));
+            }
+        }
     }
 }
